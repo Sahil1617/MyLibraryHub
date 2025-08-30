33e5cde7fb6655bf3c18518882de1afd252aa935
Sahil Jadhav <sahiljadhav1617@gmail.com>
2025-08-30 22:09:48 +0530

    Backend Added

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
+import bookRoutes from "./routes/book.js";
+import { errorHandler } from "./errorHandler.js";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGOURI);
+        console.log("MongoDB is Connected");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+    connectDB();
+});
+
+app.use("/auth", authRoutes);
+app.use("/books", bookRoutes);
+
+
+app.use(errorHandler);
